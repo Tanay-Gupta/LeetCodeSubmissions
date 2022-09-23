ad23c314f58d232ef1fdd986d9ae832ed689d282
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2022-09-24 02:26:51 +0530

    Time: 319 ms (53.25%), Space: 41.5 MB (54.55%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -3,18 +3,19 @@ class Solution {
         
         
         int mod=(int)(Math.pow(10,9))+7;
-        long i,c=0;
+        int i,c=0;
         long sum=0;
         for(i=1;i<=n;i++)
         {
             
             
-            long t=i;
+            // long t=i;
             c=0;
-            while(t>0){
-                t=t>>>1;
-                c++;
-            }
+            // while(t>0){
+            //     t=t>>>1;
+            //     c++;
+            // }
+            c=((int)(Math.log(i)/Math.log(2))+1);
             sum=((sum<<c)+i)%mod;
         }
         return((int)sum%mod);
