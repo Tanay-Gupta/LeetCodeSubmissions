bcbcfa52f1401569367667c2abb4cc8e9f270774
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2022-10-12 16:54:39 +0530

    Time: 291 ms (24.68%), Space: 68.4 MB (54.97%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,48 +1,29 @@
-// class Solution {
-//     public String removeStars(String s) {
-//         Stack<Character> stack =new Stack<>();
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> stack =new Stack<>();
         
-//         for(int i=0;i<s.length();i++)
-//         {
-//             char ch=s.charAt(i);
-//             if(!stack.isEmpty()&&ch=='*')
-//                 stack.pop();
-//             else
-//                 stack.push(ch);
-//         }
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(!stack.isEmpty()&&ch=='*')
+                stack.pop();
+            else
+                stack.push(ch);
+        }
 //         String ns="";
         
-//         while(stack.isEmpty()==false)
+//         while(!stack.isEmpty())
 //         {
 //             ns=stack.pop()+ns;
 //         }
 //         return(ns);
-        
-//     }
-// }
-class Solution {
-    public String removeStars(String s) {
-        //Creating stack
-        Stack<Character> stack =new Stack<Character>();
-        for(int i=0;i<s.length();i++){
-            //If cuurent character is * and stack is not empty then poping characters from stack 
-            if(s.charAt(i)=='*' && !stack.isEmpty()){
-                stack.pop();
-            }
-            //Else pushing character to stack
-            else{
-                stack.push(s.charAt(i));
-            }
-        }
-        //Creating Stringbuffer
         StringBuffer res=new StringBuffer();
-        while(!stack.isEmpty()){
-            //Popping the elements and pushing them to o stringbuffer
+        while(!stack.isEmpty())
+        {
             res.append(stack.pop());
         }
-        //Reversing res
         res.reverse();
-        //Returning result by converting stringbuffer to string
-        return res.toString();
+        return(res.toString());
+        
     }
 }
