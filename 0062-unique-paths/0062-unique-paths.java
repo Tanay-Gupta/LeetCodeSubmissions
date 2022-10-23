03c6c11462049b4c3af5e40f3aafca4ec7ef20d6
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2022-10-24 01:48:55 +0530

    Time: 0 ms (100.00%), Space: 41 MB (45.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,6 @@
 class Solution {
+   /*
+   * Memoration method:
    int dp[][];
     public int unique(int m, int n) {
         
@@ -18,13 +20,39 @@ class Solution {
             
     
     }
+   */
+    
+ 
     public int uniquePaths(int m, int n) {
-        dp=new int[m+1][n+1];
+        
+        /*
+        * dp=new int[m+1][n+1];
         for(int i=0;i<m;i++)
             Arrays.fill(dp[i],-1);
        
-       return(unique(m-1,n-1)) ;
+       return(unique(m-1,n-1)) ;*/
+       
             
-    
+    int dp[][]=new int[m][n];
+        int i,j;
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(i==0&&j==0)
+                {
+                     dp[i][j]=1;
+                    continue;
+                }
+                  
+                int left,up;
+                left=up=0;
+                if(j>0)left=dp[i][j-1];
+                if(i>0)up=dp[i-1][j];
+                dp[i][j]=left+up;
+            }
+                
+        }
+        return(dp[m-1][n-1]);
     }
 }
