1a013c82a3648aaccc79353eb3cb59599eb099ce
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2022-10-24 20:45:19 +0530

    Time: 5 ms (43.94%), Space: 45.7 MB (67.84%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -23,10 +23,28 @@ class Solution {
         
          dp=new int[g.length+1][g[0].length+1];
         
-        for(int i=0;i<g.length;i++)
-            Arrays.fill(dp[i],-1);
+//         for(int i=0;i<g.length;i++)
+//             Arrays.fill(dp[i],-1);
     
-         return(walk(g,g.length-1,g[0].length-1));
+//          return(walk(g,g.length-1,g[0].length-1));
+        int i,j;
+        for(i=0;i<g.length;i++)
+        {
+            for(j=0;j<g[0].length;j++)
+            {
+                if(i==0&&j==0)
+                    dp[i][j]=g[i][j];
+                else{
+                    int up,left;
+                    up=left=Integer.MAX_VALUE;
+                     if(i>0) up=dp[i-1][j]+g[i][j];
+                      if(j>0) left=dp[i][j-1]+g[i][j];
+                    dp[i][j]=Math.min(up,left);
+                }
+                
+            }
+        }
+        return(dp[g.length-1][g[0].length-1]);
         
     }
 }
