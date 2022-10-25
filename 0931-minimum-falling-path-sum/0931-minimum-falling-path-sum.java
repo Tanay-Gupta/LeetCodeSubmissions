95d2ce36f52c32d46d5080898b6b3a6311defd05
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2022-10-25 05:48:11 +0530

    Time: 14 ms (28.35%), Space: 46.6 MB (82.76%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,34 +1,60 @@
 class Solution {
     int dp[][];
-    public int walk(int a[][],int i,int j)
-    {
-        if(j<0||i<0||j>a.length-1)
-            return((int)1e9);
-        if(i==0)
-        {
-            return(a[i][j]);
-        }
+//     public int walk(int a[][],int i,int j)
+//     {
+//         if(j<0||i<0||j>a.length-1)
+//             return((int)1e9);
+//         if(i==0)
+//         {
+//             return(a[i][j]);
+//         }
       
-        if(dp[i][j]!=-200)
-            return(dp[i][j]);
-        int sameIndex,prevIndex,nextIndex;
-        sameIndex=a[i][j]+walk(a,i-1,j);
-        prevIndex=a[i][j]+walk(a,i-1,j-1);
-        nextIndex=a[i][j]+walk(a,i-1,j+1);  
-        return(dp[i][j]=Math.min(sameIndex,Math.min(prevIndex,nextIndex)));
+//         if(dp[i][j]!=-200)
+//             return(dp[i][j]);
+//         int sameIndex,prevIndex,nextIndex;
+//         sameIndex=a[i][j]+walk(a,i-1,j);
+//         prevIndex=a[i][j]+walk(a,i-1,j-1);
+//         nextIndex=a[i][j]+walk(a,i-1,j+1);  
+//         return(dp[i][j]=Math.min(sameIndex,Math.min(prevIndex,nextIndex)););
             
-    }
+//     }
     public int minFallingPathSum(int[][] matrix) {
      
         int n=matrix.length;
-        dp=new int[n+1][n+1];
-        for(int i=0;i<n+1;i++)
-            Arrays.fill(dp[i],-200);
+        dp=new int[n][n];
+        // for(int i=0;i<n+1;i++)
+        //     Arrays.fill(dp[i],-200);
+        // int min=Integer.MAX_VALUE;
+        // for(int i=0;i<n;i++)
+        // {
+        //     min=Math.min(min,walk(matrix,n-1,n-1-i));
+        // }
+        // return(min);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0)
+                    dp[i][j]=matrix[i][j];
+                else
+                {
+                      int sameIndex,prevIndex,nextIndex;
+                  sameIndex=prevIndex=nextIndex=(int)1e9;
+                              sameIndex=matrix[i][j]+dp[i-1][j];
+                    if(j-1>=0)  prevIndex=matrix[i][j]+dp[i-1][j-1];
+                    if(j+1<n)  nextIndex=matrix[i][j]+dp[i-1][j+1]; 
+                    dp[i][j]=Math.min(sameIndex,Math.min(prevIndex,nextIndex));
+                }
+            }
+        }
         int min=Integer.MAX_VALUE;
         for(int i=0;i<n;i++)
         {
-            min=Math.min(min,walk(matrix,n-1,n-1-i));
+            min=Math.min(min,dp[n-1][i]);
         }
         return(min);
+        
+        
     }
 }
