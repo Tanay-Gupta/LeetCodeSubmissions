4a138fc346059a5c27f32d6be3752ed436d82a75
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2022-11-09 07:31:43 +0530

    Time: 22 ms (99.86%), Space: 50.4 MB (91.63%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,28 @@
+class StockSpanner {
+
+    ArrayDeque<int[]> ar;
+    public StockSpanner() {  
+        ar=new ArrayDeque<>();
+  
+    }
+    
+    public int next(int price) {
+        
+        int span=1;
+        while(!ar.isEmpty()&&ar.peek()[0]<=price)
+        {
+            span+=ar.pop()[1];
+        }
+        ar.push(new int[]{price,span});
+        return(span);
+        
+        
+        
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
