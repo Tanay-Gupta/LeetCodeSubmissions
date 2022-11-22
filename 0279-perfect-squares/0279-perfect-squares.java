d5cdc0c7ee952e7325beabb6dc44d0e246bae541
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2022-11-23 01:50:29 +0530

    Time: 38 ms (81.57%), Space: 42.8 MB (72.80%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,57 +1,13 @@
-class Solution {
-    int dp[][];
-    int walk(int a[],int index,int target)
-    {
-        if(index==0)
-        {
-           if(target%a[index]==0)
-               return(target/a[index]);
-            else
-                return((int)1e9);
-        }
-        if(target==0)
-            return 0;
-        
-        if(dp[index][target]!=-1)
-            return dp[index][target];
-        
-        int notpick=walk(a,index-1,target);
-        int pick=(int)1e9;
-        if(a[index]<=target)
-            pick=1+walk(a,index,target-a[index]);
-        
-        return (dp[index][target]=Math.min(pick,notpick));
-    }
-    
-    
+public class Solution {
     public int numSquares(int n) {
-        
-       
-        
-        int i=0;
-        int x=1;
-        int a[]=new int[n];
-        while(x*x<=n)
-        {
-           a[i++]=x*x;
-            x++;          
-        }
-      
-        
-        dp=new int[i][n+1];
-        
-        for(int j=0;j<i;j++)
-            Arrays.fill(dp[j],-1);
-        
-        return walk(a,i-1,n);
-        
-        
-        
-        
-        
-        
-        
-        
-        
+       int[] dp = new int[n + 1];
+       Arrays.fill(dp, Integer.MAX_VALUE);
+       dp[0] = 0;
+       for(int i = 0; i <= n; i++){
+           for(int j = 1; i + j * j <= n; j++){
+               dp[i  + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
+            }
+       }
+       return dp[n];
     }
 }
