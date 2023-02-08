68c29a77907491f9aa4767840f890fa5065283da
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-02-08 13:19:49 +0530

    Time: 1 ms (99.85%), Space: 42.5 MB (75.62%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -2,29 +2,27 @@ class Solution {
     
     //similar to combinational sum II
     
-    public void walk(int i,int a[],Set<List<Integer>> list, List<Integer> ar)
+    public void walk(int i,int a[],List<List<Integer>> list, List<Integer> ar)
     {
-        if(i==a.length)
-        {
             list.add(new ArrayList<>(ar));
-            return;
-        }
+       
+       
         
-        ar.add(a[i]);
-        walk(i+1,a,list,ar);
-        ar.remove(ar.size()-1);
-        walk(i+1,a,list,ar);
+        for(int j=i;j<a.length;j++)
+        {
+            if(j!=i&&a[j]==a[j-1])continue;
+            ar.add(a[j]);
+            walk(j+1,a,list,ar);
+            ar.remove(ar.size()-1);
+        }
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
 
         
-        Arrays.sort(nums);
-        Set<List<Integer>> list=new HashSet<>();
+       Arrays.sort(nums);
+        List<List<Integer>> list=new ArrayList<>();
         walk(0,nums,list,new ArrayList<>());
-        List<List<Integer>> list1=new ArrayList<>(list);
-        
-        
-        return list1;
+        return list;
     
     }
 }
