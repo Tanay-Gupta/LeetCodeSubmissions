9263e268b7f6e0f2b64d70cbcde28999b6ab5b3e
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-02-23 03:48:15 +0530

    Time: 15 ms (18.76%), Space: 54.3 MB (6.48%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    
+    public int time(int a[],int c,int days)
+    {
+       int daysNeeded = 1, currentLoad = 0;
+        for(int i=0;i<a.length;i++)
+        {
+           currentLoad += a[i];
+            if (currentLoad > c) {
+                daysNeeded++;
+                currentLoad = a[i];
+            }
+            
+        }
+       
+        return(daysNeeded);
+        
+        
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        
+        
+        int e=Arrays.stream(weights).sum();
+        int s=Arrays.stream(weights).max().getAsInt();
+        while(s<=e)
+        {
+            int m=s+(e-s)/2;
+            int totalTime=time(weights,m,days);
+            if(totalTime<=days)
+            {
+                e=m-1;
+            }
+            else
+            {
+                s=m+1;
+            }
+        }
+        return s;
+        
+        
+    }
+}
