5f812ddfd4d3e7d210f87debf87e1164e03de143
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-03-03 23:51:05 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (77.00%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        int i=haystack.indexOf(needle);
+        return(i);
+        
+        
+        
+    }
+}
