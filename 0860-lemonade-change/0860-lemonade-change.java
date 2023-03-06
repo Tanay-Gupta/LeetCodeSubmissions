30bcbf070fce3e42073cde167e476dfa6500f045
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-03-06 18:24:47 +0530

    Time: 2 ms (76.62%), Space: 54.3 MB (24.85%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        
+        
+        
+        int cash[]=new int[2];
+        
+        for(int i=0;i<bills.length;i++)
+        {
+            if(bills[i]==5)
+                cash[0]++;
+            else if(bills[i]==10)
+            {
+                if(cash[0]==0)
+                    return false;
+               
+                cash[0]--;
+                cash[1]++;
+            }
+            else
+            {
+                
+              if(cash[1]>0)
+              {
+                  cash[1]--;
+                  if(cash[0]>0)
+                      cash[0]--;
+                  else
+                      return false;
+              }
+                
+                else if(cash[0]>=3)
+                {
+                    cash[0]-=3;
+                }
+                else 
+                    return false;
+
+            }
+        }
+        return true;
+        
+    }
+}
