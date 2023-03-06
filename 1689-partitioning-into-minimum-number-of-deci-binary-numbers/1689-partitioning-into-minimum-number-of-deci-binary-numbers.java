337524fc49cbbbdea0adecb73ffde6f65e6734b6
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-03-07 02:38:55 +0530

    Time: 11 ms (33.58%), Space: 43 MB (51.50%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minPartitions(String n) {
+        
+        char ch[]=n.toCharArray();
+        Arrays.sort(ch);
+        
+        return(ch[ch.length-1]-48);
+        
+        
+    }
+}
