4c77917ce172f3798ab1eb736ffb2a4ae3a85202
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-03-09 04:00:56 +0530

    Time: 1 ms (99.50%), Space: 43.6 MB (7.66%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -22,10 +22,28 @@ class Solution {
     }
     public int jump(int[] nums) {
         
-        dp=new int[nums.length+1];
-        Arrays.fill(dp,-1);
+//         dp=new int[nums.length+1];
+//         Arrays.fill(dp,-1);
         
         
-        return  walk(0,nums);
+//         return  walk(0,nums);
+        
+        //gready solution
+        
+        int ans=0;
+        int n=nums.length;
+        int currEnd=0;
+        int currFar=0;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            currFar=Math.max(currFar,i+nums[i]);
+            if(i==currEnd)
+            {
+                ans++;
+                currEnd=currFar;
+            }
+        }
+        return ans;
     }
 }
