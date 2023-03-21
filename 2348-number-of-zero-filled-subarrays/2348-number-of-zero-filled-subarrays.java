f37742e6483099a543dd8070e73f87cea3d5d676
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-03-21 10:08:14 +0530

    Time: 6 ms (34.17%), Space: 60.2 MB (27.27%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -3,37 +3,37 @@ class Solution {
         
         
         
-//     long c=0;
+    long c=0;
         
-//         long sum=0;
-//         for(int i=0;i<nums.length;i++)
-//         {
-//             if(nums[i]==0)
-//                c++;
-//             else
-//             {
-//                 sum+=(c*(c+1))/2;
-//                 c=0;
-//             }
-//         }
-//          sum+=(c*(c+1))/2;
-        
-//         return sum;
-        
-        
-        
-        
-        int j=-1;
-        long count=0;
+        long sum=0;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==0)
-                count+=i-j;
+               c++;
             else
-                j=i;
-            
+            {
+                sum+=(c*(c+1))/2;
+                c=0;
+            }
         }
-        return count;
+         sum+=(c*(c+1))/2;
+        
+        return sum;
+        
+        
+        
+        
+//         int j=-1;
+//         long count=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i]==0)
+//                 count+=i-j;
+//             else
+//                 j=i;
+            
+//         }
+//         return count;
         
     }
 }
