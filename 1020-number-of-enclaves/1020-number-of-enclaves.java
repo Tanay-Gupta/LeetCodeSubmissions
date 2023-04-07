e3ae6ef0bc090579e72fbb4ae782aac9b1f52201
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-04-07 14:35:22 +0530

    Time: 11 ms (46.65%), Space: 56.8 MB (25.33%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,55 @@
+class Solution {
+    
+    
+    public void dfs(int row,int col,boolean vis[][],int a[][])
+    {
+        vis[row][col]=true;
+        
+        int x[]={-1,0,1,0,-1};
+        int n=a.length;
+        int m=a[0].length;
+        
+        
+        for(int i=0;i<4;i++)
+        {
+            int ro=row+x[i];
+            int co=col+x[i+1];
+            if(ro>=0&&ro<n&&co>=0&&co<m&&vis[ro][co]==false&&a[ro][co]==1)
+                dfs(ro,co,vis,a);
+        }
+    }
+    public int numEnclaves(int[][] a) {
+        
+        
+        int n=a.length;
+        int m=a[0].length;
+        
+        boolean vis[][]=new boolean[n][m];
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0||j==0||i==n-1||j==m-1)
+                {
+                    if(vis[i][j]==false&&a[i][j]==1)
+                        dfs(i,j,vis,a);
+                }
+            }
+        }
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(vis[i][j]==false&&a[i][j]==1)
+                   count++;
+            }
+        }
+        return count;
+        
+        
+        
+        
+    }
+}
