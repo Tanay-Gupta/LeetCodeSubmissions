e7080366908a23bfbe30dc6ed816f692aea6a96f
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-04-25 14:27:40 +0530

    Time: 20 ms (26.19%), Space: 42.7 MB (89.88%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
@@ -1,17 +1,22 @@
 class Solution {
     public long appealSum(String s) {
-        
+        //https://assets.leetcode.com/users/images/96c9f2ad-4378-461a-bcf0-3dd9596765be_1651377614.4588175.png
         
         
         long prev[]=new long[26];
         
         long curr=0,ans=0;
-        for(int i=0;i<s.length();i++)
-        {
+//         for(int i=0;i<s.length();i++)
+//         {
             
-            curr+=i+1-(prev[s.charAt(i)-'a']);
-            prev[s.charAt(i)-'a']=i+1;// last occurence
-            ans+=curr;
+//             curr+=i+1-(prev[s.charAt(i)-'a']);
+//             prev[s.charAt(i)-'a']=i+1;// last occurence
+//             ans+=curr;
+//         }
+        Arrays.fill(prev,-1);
+         for(int i=0;i<s.length();i++){
+            prev[s.charAt(i)-'a']=i;
+            for(int  j=0;j<26;j++) ans+=(prev[j]+1);
         }
         return ans;
         
