a276ec661001f20f28f2686e5f0063c288e3aa89
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-05-02 13:25:22 +0530

    Time: 66 ms (95.77%), Space: 62.4 MB (27.89%) - LeetHub

--- 2659-make-array-empty/2659-make-array-empty.java
+++ 2659-make-array-empty/2659-make-array-empty.java
@@ -8,6 +8,17 @@ class Solution {
             map.put(a[i],i);
         long ans=n;
         Arrays.sort(a);
+        
+        
+        /*
+        We iterate through the array. Whether we remove an item or move to the next item the cost of the operation is always 1, thus n operations.
+
+When we find an item which index is smaller than the previous, it indicates we have iterated through the entire array and must repeat the previous step again. However this time the lenght of the array is no longer same since some of the items got removed.
+
+The new lenght of the array will be n - number of removed items, thus n - i
+        */
+        
+        
         for(int i=1;i<n;i++)
         {
             if(map.get(a[i])<map.get(a[i-1]))
