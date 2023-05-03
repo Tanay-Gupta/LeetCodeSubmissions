9e45c3d4416653768ec0b4a42ba2d6e761974732
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-05-04 00:57:57 +0530

    Time: 7 ms (40.66%), Space: 43.6 MB (14.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,6 +1,12 @@
 class Solution {
     
-    public boolean dfs(boolean vis[],boolean pathvis[], ArrayList<ArrayList<Integer>> adj,int node)
+    
+    
+    /* 
+    
+    // Using cycle detection method in directed graph
+    
+     public boolean dfs(boolean vis[],boolean pathvis[], ArrayList<ArrayList<Integer>> adj,int node)
     {
         
        
@@ -49,6 +55,63 @@ class Solution {
         
         
 
+        
+    }
+    */
+    public boolean canFinish(int n, int[][] a) {
+        
+        
+        // using topo logical sort method
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++)
+            adj.add(new ArrayList<Integer>());
+        
+        for(int i=0;i<a.length;i++)
+            adj.get(a[i][0]).add(a[i][1]);
+        
+        
+        // topo sort algo using khan's 
+        
+        
+        int indegree[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            for(int it:adj.get(i))
+                indegree[it]++;
+        }
+        
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+                q.add(i);
+        }
+        
+        
+        List<Integer> topo=new ArrayList<>();
+        //O(v+e)
+        
+        while(!q.isEmpty())
+        {
+            int node=q.poll();
+            
+            topo.add(node);
+            for(int i:adj.get(node))
+            {
+                indegree[i]--;
+                if(indegree[i]==0)
+                    q.add(i);
+            }
+            
+        }
+        if(topo.size()==n)
+            return true;
+        else
+            return false;
+        
+        
+     
+        
         
     }
 }
