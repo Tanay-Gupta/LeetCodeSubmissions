ca824836292a57e9d2b4110dc7c46414980875f5
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-05-23 22:44:34 +0530

    Time: 15 ms (50.02%), Space: 47.6 MB (6.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -15,6 +15,8 @@ class Solution {
             a[x][0]=key;
             a[x++][1]=value;
         }
+        // PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->hash_map.get(b) - hash_map.get(a));
+  //  pq.addAll(hash_map.keySet());
         Arrays.sort(a,(q,y)->Integer.compare(y[1],q[1]));
         int b[]=new int[k];
         for(int i=0;i<k;i++ )
