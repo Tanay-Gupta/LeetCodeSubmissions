3a3d8359cfd388591a31e64a72a49a7d41384cba
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-07-01 23:06:05 +0530

    Time: 13 ms (95.71%), Space: 43.3 MB (35.53%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -4,27 +4,60 @@ class Solution {
         
         
         
+//         int i,j,k;
+//         int m=(int)1e9;
+//         int c=0;
+//         for(i=0;i<a.length-2;i++)
+//         {
+//             for(j=i+1;j<a.length-1;j++)
+//             {
+//                 for(k=j+1;k<a.length;k++)
+//                 {
+//                     if(i!=j&&j!=k&&k!=i)
+//                     {
+                        
+//                             int s=a[i]+a[j]+a[k];
+//                             if(Math.abs(s-t)<m)
+//                             {
+//                                 m=Math.abs(s-t);
+//                                 c=s;
+//                             }
+//                     }
+//                 }
+//             }
+//         }
+//         return c;
+        
+        
+    
+        Arrays.sort(a);
         int i,j,k;
-        int m=(int)1e9;
+                int m=(int)1e9;
         int c=0;
         for(i=0;i<a.length-2;i++)
         {
-            for(j=i+1;j<a.length-1;j++)
+            
+            j=i+1;
+            k=a.length-1;
+            
+            while(j<k)
             {
-                for(k=j+1;k<a.length;k++)
-                {
-                    if(i!=j&&j!=k&&k!=i)
-                    {
-                        
-                            int s=a[i]+a[j]+a[k];
-                            if(Math.abs(s-t)<m)
+                int s=a[i]+a[j]+a[k];
+                if(s==t)
+                    return t;
+               else if(s<t)
+                    j++;
+                else
+                    k--;
+                    
+                    
+                if(Math.abs(s-t)<m)
                             {
                                 m=Math.abs(s-t);
                                 c=s;
-                            }
-                    }
-                }
+                            }    
             }
+        
         }
         return c;
         
