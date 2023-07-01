f37efa5a2bae0831ba141d74edd5ead70305023d
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-07-01 21:02:41 +0530

    Time: 1567 ms (5.01%), Space: 43 MB (52.39%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int threeSumClosest(int[] a, int t) {
+        
+        
+        
+        
+        int i,j,k;
+        int m=(int)1e9;
+        int c=0;
+        for(i=0;i<a.length-2;i++)
+        {
+            for(j=i+1;j<a.length-1;j++)
+            {
+                for(k=j+1;k<a.length;k++)
+                {
+                    if(i!=j&&j!=k&&k!=i)
+                    {
+                        
+                            int s=a[i]+a[j]+a[k];
+                            if(Math.abs(s-t)<m)
+                            {
+                                m=Math.abs(s-t);
+                                c=s;
+                            }
+                    }
+                }
+            }
+        }
+        return c;
+        
+    }
+}
