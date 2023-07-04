2f85573c242d576483e599de22de6777bbb3eae8
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-07-04 07:49:23 +0530

    Time: 15 ms (54.68%), Space: 47.8 MB (57.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,27 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {  
+    public int[] topKFrequent(int[] nums, int k) {
+       
+        
         
         HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i:nums)
-            map.put(i,map.getOrDefault(i,0)+1);
         
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
         
         int a[][]=new int[map.size()][2];
         int x=0;
         for(Map.Entry<Integer,Integer> i:map.entrySet())
         {
-            int key=i.getKey();
+            
+        int key=i.getKey();
             int value=i.getValue();
             a[x][0]=key;
             a[x++][1]=value;
         }
-        // PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->hash_map.get(b) - hash_map.get(a));
-  //  pq.addAll(hash_map.keySet());
+     
         Arrays.sort(a,(q,y)->Integer.compare(y[1],q[1]));
         int b[]=new int[k];
         for(int i=0;i<k;i++ )
@@ -24,6 +29,7 @@ class Solution {
             b[i]=a[i][0];
         }
         return b;
+        
         
     }
 }
