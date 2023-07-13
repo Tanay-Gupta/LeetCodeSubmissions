577e436d87cf08b574b869dd8315cd1676c8b0c2
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2023-07-13 18:24:03 +0530

    Time: 4 ms (45.10%), Space: 56.1 MB (67.83%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,27 +1,32 @@
 class Solution {
-    
-    
-    public boolean walk(int a[],int i)
-    {
-        if(i<0||i>=a.length||a[i]<0) return false;
+    public boolean helper(int idx,int[] arr,int[] dp){
+        if(idx>=arr.length || idx<0){
+            return false;
+        }
+        if(arr[idx]==0){
+            return true;
+        }
+        // if(dp[idx]!=-1){
+        //     return dp[idx]==1?true:false;
         
-        if(a[i]==0)return true;
-        
-        a[i]=-a[i];
-        return(walk(a,i+a[i])||walk(a,i-a[i]));
-        
-        
+        if(dp[idx]!=-1){
+            return false;
+        }
+        dp[idx]=1;
+        if(helper(idx+arr[idx],arr,dp)){
+            
+            return true;
+        }
+        if(helper(idx-arr[idx],arr,dp)){
+
+            return true;
+        }
+        dp[idx]=0;
+        return false;
     }
     public boolean canReach(int[] arr, int start) {
-        
-        
-        
-        
-        if(arr[start]==0)
-            return true;
-        
-        return walk(arr,start);
-        
-        
+        int[] dp=new int[arr.length];
+        Arrays.fill(dp,-1);
+        return helper(start,arr,dp);
     }
 }
