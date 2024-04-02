e6d1eb0ccca53a4951045ac47796f167b467325b
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2024-04-02 12:11:40 +0530

    Time: 107 ms (47.13%) | Memory: 46 MB (5.95%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,12 +1,42 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        return ways(nums, 0, target);
+    Map<String, Integer> map = new HashMap<>();
+    
+    public int findTargetSumWays(int[] nums, int S) {
+        
+        return helper(nums, S, 0, 0);
+        
     }
     
-    public int ways(int[] nums, int index, int target) {
-        if (index == nums.length) {
-            return target == 0 ? 1 : 0;
+    private int helper(int []nums, int targetSum, int index, int sum){
+        
+        //Base condition when sum == targetSum. we return 1 
+        //as we need to find the number of ways we can reach targetSum
+        if(index == nums.length && sum == targetSum){
+            return 1;
         }
-        return ways(nums, index + 1, target - nums[index]) + ways(nums, index + 1, target + nums[index]);
+        //Base condition when sum != targetSum . we return 0 from here
+        //as we do not want this case in final answer
+        if(index == nums.length && sum != targetSum){
+            return 0;
+        }
+        
+        //memoization using map.
+        //used immutabality property of String for unique key in map.
+        String key = sum + "()" + index;
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+        
+        int count = 0;
+        //first case we take negative value of current index
+        count += helper(nums, targetSum, index + 1, sum - nums[index]);
+        //second case we take positive value of current index
+        count += helper(nums, targetSum , index + 1, sum + nums[index]);
+        
+        //add to map for future use.
+        map.put(key, count);
+        
+        return count;
+        
     }
 }
