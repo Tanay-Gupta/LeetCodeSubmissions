424fb11094085a73843d7e4a5ed72214b9c1643c
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2024-07-20 22:15:46 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (40.82%) - LeetSync

--- 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.java
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.java
@@ -4,7 +4,7 @@ class Solution {
        char ch[]=s.toCharArray();
 
        for(int i=0;i<s.length()-1;i++){
-        if(((ch[i]-'0')%2==(ch[i+1]-'0')%2) && ch[i]>ch[i+1])
+        if(ch[i]%2==ch[i+1]%2 && ch[i]>ch[i+1])
         {
             char t=ch[i+1];
             ch[i+1]=ch[i];
