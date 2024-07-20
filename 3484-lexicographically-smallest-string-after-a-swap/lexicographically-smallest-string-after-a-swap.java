34f68f95050ea6f26afb231b0a111ee702495751
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2024-07-20 22:09:11 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (40.82%) - LeetSync

---
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String getSmallestString(String s) {
+
+       char ch[]=s.toCharArray();
+
+       for(int i=0;i<s.length()-1;i++){
+        if(((ch[i]-'0')%2==(ch[i+1]-'0')%2) && ch[i]>ch[i+1])
+        {
+            char t=ch[i+1];
+            ch[i+1]=ch[i];
+            ch[i]=t;
+            break;
+        }
+       }
+
+       return new String(ch);
+        
+    }
+}
