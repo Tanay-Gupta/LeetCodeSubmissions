a491b0a58ae329eb4267ccf629a7a40938794264
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2024-09-12 22:27:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (54.70%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int search(int[] a, int k) {
+
+        int n = a.length;
+        int s = 0, e = n - 1;
+
+        while (s <= e) {
+
+            int m = s + (e - s) / 2;
+            if (a[m] == k)
+                return m;
+
+            if (a[s] <= a[m]) {
+
+                if (a[s] <= k && a[m] >= k)
+                    e = m - 1;
+                else
+                    s = m + 1;
+
+            }
+            else
+            {
+               if (a[m] <= k && a[e] >= k)
+                    s = m + 1;
+                else
+                    e = m - 1;  
+            }
+        }
+        return -1;
+
+    }
+}
