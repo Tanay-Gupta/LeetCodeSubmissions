7887fa967c1e4a5e9d358f11eea0aabfd5c1b58a
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2025-05-27 02:29:02 +0530

    Time: 38 ms (77.88%) | Memory: 53.8 MB (54.81%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -3,5 +3,10 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-return Object.keys(obj).length==0;    
+// return Object.keys(obj).length==0;    
+ if (Array.isArray(obj)) {
+    return obj.length === 0;
+  } else {
+    return Object.keys(obj).length === 0;
+  }
 };
