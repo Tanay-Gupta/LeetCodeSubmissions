a396165a454d53d2fea8825027080b5d0ed47875
Tanay Gupta <46367369+Tanay-Gupta@users.noreply.github.com>
2025-06-01 13:02:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (91.30%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,37 +1,38 @@
 class Solution {
-    public boolean search(int[] a, int k) {
-        int n = a.length;
-        int s = 0, e = n - 1;
+    public boolean search(int[] a, int t) {
+
+        int s = 0;
+        int e = a.length - 1;
 
         while (s <= e) {
 
             int m = s + (e - s) / 2;
-            if (a[m] == k)
+
+            if (a[m] == t)
                 return true;
-
-
+           
             if(a[s]==a[m]&&a[m]==a[e]){//endge case for repeated elements
                 s++;
                 e--;
                 continue;
             }
-            if (a[s] <= a[m]) {
+            else if (a[s] <= a[m]) {// left half is sorted
 
-                if (a[s] <= k && a[m] >= k)
+                if (a[s] <= t && t < a[m])
                     e = m - 1;
                 else
                     s = m + 1;
 
-            }
-            else
-            {
-               if (a[m] <= k && a[e] >= k)
-                    s = m + 1;
+            } else {
+                if (t>a[m]&&t<=a[e])
+                    s=m+1;
                 else
-                    e = m - 1;  
+                    e=m-1;
+
             }
         }
 
         return false;
+
     }
 }
